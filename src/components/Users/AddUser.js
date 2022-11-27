68e4965fead9e247f4c79663980e071fd373e066
zainab memon <zainabsafi23@gmail.com>
2022-11-28 00:47:38 +0500

    second commit

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -45,7 +45,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <div>
+    <React.Fragment>
       {error && (
         <ErrorModal
           title={error.title}
@@ -72,7 +72,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </React.Fragment>
   );
 };
 export default AddUser;
